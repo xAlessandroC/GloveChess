5b1678ce7d4d953aff0f48a03e3192445b95d1cd
xAlessandroC <alexcalvio@hotmail.it>
2020-03-06 19:03:46 +0100

    Add support for execution of moves

---
+++ model/executer.py
@@ -0,0 +1,35 @@
+from Chessboard import *
+from model_utils import *
+from queenChecker import *
+
+def checkAndExecuteMove(fromCell, toCell):
+    chessboard = Chessboard.getInstance()
+
+    currentTurn = Player(chessboard.get_turn()[1]).name
+    selectedPiece = chessboard.from_index_to_piece(fromCell)
+
+    #Controllo che venga mosso un pezzo del giocatore di cui è il turno
+    print("### EXECUTER ###\nCurrent Turn:",currentTurn,"\nSelectedPiece:", selectedPiece)
+    print("Chessboard from:", fromCell,"\nMatrix from:",chessboard.from_chessboard_to_matrix(fromCell))
+    print("Chessboard to:", toCell,"\nMatrix to:",chessboard.from_chessboard_to_matrix(toCell))
+
+    if selectedPiece[0] != currentTurn[0] :
+        raise Exception("[WrongTurnException]: Non stai muovendo un tuo pezzo")
+
+
+    #Controllo che il pezzo rispetti le proprie regole
+    checkerName = selectedPiece[2:].lower() + "Checker"
+    module = __import__(checkerName)
+    class_ = getattr(module, checkerName[0].upper()+checkerName[1:])
+    instance = class_()
+
+    if instance.checkMove(fromCell, toCell):
+        ##Aggiorno chessboard
+        chessboard.update(fromCell,toCell)
+        return True
+
+    return False
+
+
+Chessboard.getInstance()
+print(checkAndExecuteMove("d5","b7"))
