099afa7f4430eb41b0a60e501d3da926b3295b41
xAlessandroC <alexcalvio@hotmail.it>
2020-03-23 11:31:02 +0100

    minor changes

--- utils/config.py
+++ utils/config.py
@@ -3,7 +3,12 @@ test_path = "resources/marker/image_1.jpg"
 video_path = "resources/marker/videooh_2.mp4"
 model_path = "resources/marker/aruco/model_50.png"
 obj_path = "resources/chess_models/Scacchiera/10586_Chess_Board_v2_Iterations-2.obj"
-obj_path2 = "resources/chess_models/Alfiere_Bianco/12929_WoodenChessBishopSideA_v1_l3.obj"
+# obj_path2 = "resources/chess_models/Alfiere_Bianco/12929_WoodenChessBishopSideA_v1_l3.obj"
+# obj_path2 = "resources/chess_models/Alfiere_Bianco/reduced.obj"
+obj_path2 = "resources/chess_models_reduced/Torre_Nera/reduced_3k.obj"
+# obj_path2 = "resources/chess_models/Cube/cube.obj"
+
+
 # obj_path2 = "resources/chess_models/Cube/cube.obj"
 
 
