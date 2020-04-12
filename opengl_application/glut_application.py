d5535012f4e9d25afca8f8ae09a964cdb1c47862
xAlessandroC <alexcalvio@hotmail.it>
2020-04-12 11:27:23 +0200

    Add pieces movement with mouse interaction

--- opengl_application/glut_application.py
+++ opengl_application/glut_application.py
@@ -16,6 +16,7 @@ from webcam import *
 from utility import *
 from opengl_utils import *
 from findCenters import *
+from mouse_interaction import *
 from chessboard import *
 from executer import *
 from piece import *
@@ -52,36 +53,6 @@ selector_y = 0
 
 current_mvm = None
 ###############################
-def mouse(button, state, x, y):
-    global obj_s
-    x_ndc = (2.0 * x + 1.0) / 1280 - 1.0
-    y_ndc = (2.0 * y + 1.0) / 720 - 1.0
-
-    modelMatrix_r = glGetDoublev(GL_MODELVIEW_MATRIX)
-    projMatrix_r = glGetDoublev(GL_PROJECTION_MATRIX)
-    viewport_r = glGetIntegerv(GL_VIEWPORT)
-
-    # print(modelMatrix_r)
-    # print(projMatrix_r)
-    # print(viewport_r)
-
-    if state == GLUT_DOWN:
-        z_value = glReadPixels( x, 720-y, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT)
-        world_coord = gluUnProject(x, 720-y, z_value, current_mvm, projMatrix_r, viewport_r)
-        world_coord = (world_coord[0], -world_coord[1], -world_coord[2])
-
-        center_idx = nearestCenter(centers, *world_coord)
-        print("CENTER_IDX:", center_idx)
-        if center_idx[0]>0 and center_idx[1]>0:
-            new_vertices = translateVertices(pieces_data.id_selectionSprite, obj_s, *tuple(centers[center_idx[0], center_idx[1]]), z=13)
-            overwriteList(pieces_data.id_selectionSprite, obj_s, new_vertices)
-
-        print("MVM:",current_mvm)
-        print("[VIEWPORT]: Button click at ",x,y)
-        print("Z-VALUE:", z_value)
-        print("3D COORD:", world_coord)
-        # print("[NDC]: Button click at ",x_ndc,y_ndc)
-
 
 def keyboard(key, x, y):
     global selector_x, selector_y, obj_s
