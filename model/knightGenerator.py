20eb04c90fb7cfae59ca0f2d50727ab3b0de7d6b
xAlessandroC <alexcalvio@hotmail.it>
2020-04-28 18:32:16 +0200

    Change checker to generator

---
+++ model/knightGenerator.py
@@ -0,0 +1,33 @@
+from chessboard import Chessboard
+from model_utils import *
+from generator import *
+import math
+
+class KnightGenerator(Generator):
+
+    def generateMoves(self, fromCell):
+        print("### KNIGHT GENERATOR ###")
+
+        moves = []
+        chessboard = Chessboard.getInstance()
+        pieces = chessboard.getPieces()
+        currentTurn = Turn(chessboard.get_turn()[1]).name[0]
+
+        fromCell_matrix = from_chessboard_to_matrix(fromCell)
+
+        possibleMoves = []
+        possibleMoves.append( (fromCell_matrix[0] + 2, fromCell_matrix[1] + 1) ) # L up-dx
+        possibleMoves.append( (fromCell_matrix[0] + 2, fromCell_matrix[1] - 1) ) # L up-sx
+        possibleMoves.append( (fromCell_matrix[0] - 2, fromCell_matrix[1] + 1) ) # L down-dx
+        possibleMoves.append( (fromCell_matrix[0] - 2, fromCell_matrix[1] - 1) ) # L down-sx
+        possibleMoves.append( (fromCell_matrix[0] + 1, fromCell_matrix[1] + 2) ) # L dx-up
+        possibleMoves.append( (fromCell_matrix[0] - 1, fromCell_matrix[1] + 2) ) # L dx-down
+        possibleMoves.append( (fromCell_matrix[0] + 1, fromCell_matrix[1] - 2) ) # L sx-up
+        possibleMoves.append( (fromCell_matrix[0] - 1, fromCell_matrix[1] - 2) ) # L sx-down
+
+        for to_cell in possibleMoves:
+            if to_cell[0]>= 0 and to_cell[0]<8 and to_cell[1]>= 0 and to_cell[1]<8 and
+               (pieces[*to_cell].name.startsWith(currentTurn) == False or pieces[*to_cell] == Piece.EMPTY):
+                moves.append(to_cell)
+
+        return moves
---
+++ model/pawnGenerator.py
@@ -0,0 +1,48 @@
+from chessboard import *
+from model_utils import *
+from generator import *
+import math
+
+class PawnGenerator(Generator):
+
+    __B_startPosition = ["a7","b7","c7","d7","e7","f7","g7","h7"]
+    __W_startPosition = ["a2","b2","c2","d2","e2","f2","g2","h2"]
+
+    def generateMoves(self, fromCell):
+        print("### PAWN GENERATOR ###")
+
+        moves = []
+        chessboard = Chessboard.getInstance()
+        pieces = chessboard.getPieces()
+        currentTurn = Turn(chessboard.get_turn()[1]).name[0]
+
+        fromCell_matrix = from_chessboard_to_matrix(fromCell)
+
+        limit = 1
+        direction = 0
+        if currentTurn == "W" and fromCell in self.__W_startPosition:
+            direction = 1
+            limit = 2
+
+        if currentTurn == "B" and fromCell in self.__B_startPosition:
+            direction = -1
+            limit = 2
+
+        climb = False
+        for i in range(1, limit+1):
+            to_cell = (fromCell_matrix[0], fromCell_matrix[1] + (i*direction))
+            if to_cell[0]>= 0 and to_cell[0]<8 and to_cell[1]>= 0 and to_cell[1]<8 and climb==False:
+                if pieces[*to_cell].name.startsWith(currentTurn) == Piece.EMPTY:
+                    moves.append(to_cell)
+                if pieces[*to_cell].name.startsWith(currentTurn) != Piece.EMPTY:
+                    climb = True
+
+        to_cell = (fromCell_matrix[0] + 1, fromCell_matrix[1] + direction)
+        if to_cell[0]>= 0 and to_cell[0]<8 and to_cell[1]>= 0 and to_cell[1]<8 and pieces[*to_cell].name.startsWith(currentTurn) == False:
+            moves.append(to_cell)
+
+        to_cell = (fromCell_matrix[0] - 1, fromCell_matrix[1] + direction)
+        if to_cell[0]>= 0 and to_cell[0]<8 and to_cell[1]>= 0 and to_cell[1]<8 and pieces[*to_cell].name.startsWith(currentTurn) == False:
+            moves.append(to_cell)
+
+        return moves
