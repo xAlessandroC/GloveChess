55ea944fea9228dd3038ccfa243dcf4a13a2a3ff
xAlessandroC <alexcalvio@hotmail.it>
2020-05-01 17:13:41 +0200

    Improved center computation and threshold

--- finger.py
+++ finger.py
@@ -14,6 +14,23 @@ low_H = 0;  high_H = 50
 low_S = 13;  high_S = 190
 low_V = 20;  high_V = 255
 
+def getReduction(ar):
+    armax = 0.70
+    armin = 0.30
+    vmax = 230
+    vmin = 100
+
+    x = ar
+    if ar>armax:    x=armax
+    if ar<armin:    x=armin
+
+    r1 = (vmin-vmax)/(armax-armin)
+    r2 = (armin*(vmin-vmax))/(armax-armin)
+
+    reduction = x*r1 - r2 + vmax
+    reduction = int(reduction)
+    # print("AR:",ar,"Reduction for",x,"=",reduction)
+    return reduction
 
 def findFingers(frame, contour, hull):
 
@@ -21,9 +38,9 @@ def findFingers(frame, contour, hull):
     defects = cv2.convexityDefects(contour, hull_idx)
 
     if (defects is None) == False:
-        fingers_top = getAllFingerTop(defects, contour)
+        fingers_top, _far = getAllFingerTop(defects, contour)
 
-        filtered_fingers, bounding_r = fingerFilter(frame, fingers_top, contour, hull)
+        filtered_fingers, bounding_r = fingerFilter(frame, fingers_top, contour, hull, _far)
 
         for finger in filtered_fingers:
             cv2.circle(frame, finger, 8, [255, 0, 0], -1);
@@ -32,10 +49,39 @@ def findFingers(frame, contour, hull):
     else:
         return ([],[])
 
-def fingerFilter(contoured_frame, fingers, contour, hull):
+def giveCenter(fingers, bounding_r, _far):
+    k = -1
+    ar = None
+    if bounding_r[2]>= bounding_r[3]:
+        k = 0
+        ar = bounding_r[3]/bounding_r[2]
+    else:
+        k = 1
+        ar = bounding_r[2]/bounding_r[3]
+
+    center = [int(bounding_r[0]+bounding_r[2]/2), int(bounding_r[1]+bounding_r[3]/2)]
+    max = 0
+    min = 0
+    for i in range(len(_far)):
+        if _far[i][k] > center[k]:
+            max = max + 1
+        else:
+            min = min + 1
+
+    if max >= min:
+        center[k] = center[k] + getReduction(ar)
+    else:
+        center[k] = center[k] - getReduction(ar)
+
+    # point = (bounding_r[0]+(bounding_r[2]*), bounding_r[1]+(bounding_r[3]*))
+
+    return (center[0], center[1])
+
+def fingerFilter(contoured_frame, fingers, contour, hull, _far):
 
     bounding_r = cv2.boundingRect(hull)
-    center = (int(bounding_r[0] + bounding_r[2] / 2), int(bounding_r[1] + bounding_r[3] / 2))
+    # center = (int(bounding_r[0] + bounding_r[2] / 2), int(bounding_r[1] + bounding_r[3] / 2))
+    center = giveCenter(fingers, bounding_r, _far)
     cv2.rectangle(contoured_frame, (int(bounding_r[0]), int(bounding_r[1])), (int(bounding_r[0] + bounding_r[2]), int(bounding_r[1] + bounding_r[3])), (255,0,0), 2)
     cv2.circle(contoured_frame, center, 8, [211, 84, 0], -1)
 
@@ -52,6 +98,7 @@ def fingerFilter(contoured_frame, fingers, contour, hull):
 def getAllFingerTop(defects, selected_cnt):
     fingers = []
     temp = []
+    _far = []
 
     # Aggiungo le coppie (start, end) di tutti i punti di convessità che hanno un angolo minore di 90°
     for i in range(len(defects)):
@@ -67,6 +114,7 @@ def getAllFingerTop(defects, selected_cnt):
 
         if angle <= math.pi / 2:
             temp.append([start,end])
+            _far.append(far)
 
     # Da tutte le coppie (start, end) isolo quelle con valori molto diversi
     for i in range(len(temp)):
@@ -82,7 +130,7 @@ def getAllFingerTop(defects, selected_cnt):
             fingers.append(temp[i][0])
             fingers.append(temp[next_idx][1])
 
-    return fingers
+    return fingers, _far
 
 ##################### MAIN FUNCTION ########################
 def finger_detection(frame):
